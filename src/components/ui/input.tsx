8b5303e90f4f1daa11c81d71bbd39c6a37c5f76f
raffi23 <raffi@rhymecode.net>
2025-05-07 19:18:08 +0400

    add: input

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,23 @@
+import { cx } from "@/utils";
+import { FC, InputHTMLAttributes } from "react";
+
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
+  label?: string;
+}
+
+const Input: FC<Props> = ({ label, className, ...rest }) => {
+  return (
+    <div className="flex flex-col gap-1">
+      {label && <label className="text-sm">{label}</label>}
+      <input
+        className={cx(
+          "min-w-52 border rounded-md h-9 p-2 focus-visible:outline-gray-600",
+          className
+        )}
+        {...rest}
+      />
+    </div>
+  );
+};
+
+export default Input;
