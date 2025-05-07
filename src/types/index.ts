342e1eb153f98ad5dc26539a53b3bfb276919ce2
raffi23 <raffi@rhymecode.net>
2025-05-07 12:09:29 +0400

    add: types

---
+++ src/types/index.ts
@@ -0,0 +1,10 @@
+export type EventListing = {
+  id: number;
+  title: string;
+  location: string;
+  description: string;
+  type: string;
+  starts_at: string;
+  expires_at: string;
+  image_url: string;
+};
