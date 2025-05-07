01db4a5a38331a83fea48ede850b939522b2aaf5
raffi23 <raffi@rhymecode.net>
2025-05-07 20:13:30 +0400

    add: back home link

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -2,6 +2,7 @@ import { getEvents } from "@/lib/api";
 import EventsTable from "./_components/events-table";
 import { PropsWithParams } from "@/types";
 import { eventQuerySchema } from "@/utils/validations";
+import NextLink from "@/components/ui/link";
 
 export default async function EventsPage({ searchParams }: PropsWithParams) {
   const { data: query } = eventQuerySchema.safeParse(await searchParams);
@@ -9,7 +10,15 @@ export default async function EventsPage({ searchParams }: PropsWithParams) {
 
   return (
     <div className="flex flex-col gap-5">
-      <h1 className="text-4xl font-bold">Event listings</h1>
+      <div className="flex gap-4 items-center">
+        <NextLink
+          href="/"
+          className="text-2xl size-9 flex items-center justify-center hover:bg-gray-100 border rounded"
+        >
+          {"<"}
+        </NextLink>
+        <h1 className="text-4xl font-bold">Event listings</h1>
+      </div>
       <EventsTable events={events} />
     </div>
   );
