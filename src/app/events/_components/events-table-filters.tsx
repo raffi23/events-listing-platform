cc9b9a2eb540137d578935cf9720cf9ca7837508
raffi23 <raffi@rhymecode.net>
2025-05-07 22:42:15 +0400

    add: clear button on query filters

--- src/app/events/_components/events-table-filters.tsx
+++ src/app/events/_components/events-table-filters.tsx
@@ -6,6 +6,7 @@ import { TableToolbar } from "@/components/ui/table";
 import useDebounce from "@/hooks/useDebounce";
 import { eventQuerySchema } from "@/utils/validations";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 import { z } from "zod";
 
 type QuerySchema = z.infer<typeof eventQuerySchema>;
@@ -13,9 +14,9 @@ type QuerySchema = z.infer<typeof eventQuerySchema>;
 const EventsTableFilters = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  const currentValues = Object.fromEntries(
-    searchParams.entries()
-  ) as QuerySchema;
+  const [currentValues, setCurrentValues] = useState<QuerySchema>(
+    Object.fromEntries(searchParams.entries())
+  );
 
   const debounceChangeHandler = useDebounce((updatedQuery) => {
     router.push(`/events?${updatedQuery}`);
@@ -30,6 +31,7 @@ const EventsTableFilters = () => {
       [key]: value || "",
     });
     const updatedQuery = new URLSearchParams(query);
+    setCurrentValues(Object.fromEntries(updatedQuery.entries()));
     debounceChangeHandler(updatedQuery);
   };
 
@@ -38,31 +40,34 @@ const EventsTableFilters = () => {
       <Input
         label="Title"
         placeholder="Search by title"
-        defaultValue={decodeURIComponent(currentValues?.title || "")}
+        value={decodeURIComponent(currentValues?.title || "")}
         onChange={(e) => handleQueryChange("title", e.target.value)}
+        onClear={() => handleQueryChange("title", "")}
       />
       <Input
         label="Location"
         placeholder="Search by location"
-        defaultValue={decodeURIComponent(currentValues?.location || "")}
+        value={decodeURIComponent(currentValues?.location || "")}
         onChange={(e) => handleQueryChange("location", e.target.value)}
+        onClear={() => handleQueryChange("location", "")}
       />
       <Input
         label="Category"
         placeholder="Search by category"
-        defaultValue={decodeURIComponent(currentValues?.type || "")}
+        value={decodeURIComponent(currentValues?.type || "")}
         onChange={(e) => handleQueryChange("type", e.target.value)}
+        onClear={() => handleQueryChange("type", "")}
       />
       <DatePicker
         label="Start at"
         placeholder="Select date"
-        defaultValue={decodeURIComponent(currentValues?.starts_at || "")}
+        value={decodeURIComponent(currentValues?.starts_at || "")}
         onChange={(e) => handleQueryChange("starts_at", e.target.value)}
       />
       <DatePicker
         label="Ends at"
         placeholder="Select date"
-        defaultValue={decodeURIComponent(currentValues?.expires_at || "")}
+        value={decodeURIComponent(currentValues?.expires_at || "")}
         onChange={(e) => handleQueryChange("expires_at", e.target.value)}
       />
     </TableToolbar>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,39 @@
+"use client";
+
 import { cx } from "@/utils";
-import { FC, InputHTMLAttributes } from "react";
+import { XIcon } from "lucide-react";
+import { FC, InputHTMLAttributes, MouseEventHandler, useRef } from "react";
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   label?: string;
+  onClear?: MouseEventHandler<HTMLButtonElement>;
 }
 
-const Input: FC<Props> = ({ label, className, ...rest }) => {
+const Input: FC<Props> = ({ label, onClear, value, className, ...rest }) => {
+  const ref = useRef<HTMLInputElement | null>(null);
+
   return (
     <div className="flex flex-col gap-1">
       {label && <label className="text-sm">{label}</label>}
-      <input
-        className={cx(
-          "w-full sm:min-w-52 border rounded-md h-9 p-2 focus-visible:outline-gray-600 dark:bg-gray-800",
-          className
+      <div className="relative">
+        <input
+          ref={ref}
+          value={value}
+          className={cx(
+            "w-full sm:min-w-52 border rounded-md h-9 p-2 focus-visible:outline-gray-600 dark:bg-gray-800",
+            className
+          )}
+          {...rest}
+        />
+        {value && (
+          <button
+            className="absolute p-px rounded transition-colors hover:dark:text-black hover:dark:bg-white hover:text-white hover:bg-gray-800 top-1/2 right-2 -translate-y-1/2"
+            onClick={onClear}
+          >
+            <XIcon size={14} className="pointer-events-none" />
+          </button>
         )}
-        {...rest}
-      />
+      </div>
     </div>
   );
 };
