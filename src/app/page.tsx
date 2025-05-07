85499b4b200ae8ea28000fce3081af7c4f413762
raffi23 <raffi@rhymecode.net>
2025-05-07 22:42:50 +0400

    fix: center text on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import NextLink from "@/components/ui/link";
 export default async function Home() {
   return (
     <div className="flex items-center justify-center h-[calc(100svh_-_2rem)] xl:h-[100vh_-_2rem]">
-      <NextLink href="/events" className="text-3xl w-fit">
+      <NextLink href="/events" className="text-3xl w-fit text-center">
         Click here to view Events Listing
       </NextLink>
     </div>
