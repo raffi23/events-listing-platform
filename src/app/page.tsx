2864028098aea5dde706b4ad68aadc972ad0293b
raffi23 <raffi@rhymecode.net>
2025-05-07 20:05:10 +0400

    fix: height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import NextLink from "@/components/ui/link";
 
 export default async function Home() {
   return (
-    <div className="flex items-center justify-center h-svh xl:h-screen">
+    <div className="flex items-center justify-center h-[calc(100svh_-_2rem)] xl:h-[100vh_-_2rem]">
       <NextLink href="/events" className="text-3xl w-fit">
         Click here to view Event Listings
       </NextLink>
