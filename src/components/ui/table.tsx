3f7634fa44d5d02e6b591fb35f8d686a0d6a257e
raffi23 <raffi@rhymecode.net>
2025-05-08 10:54:04 +0400

    chore: update table

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -21,7 +21,7 @@ const TableToolbar: FC<HTMLAttributes<HTMLDivElement>> = ({
   return (
     <div
       className={cx(
-        "sticky top-0 grid grid-cols-2 sm:flex sm:items-center gap-2 flex-wrap bg-white dark:bg-gray-800 rounded-b-md p-4 border-b",
+        "sticky top-0 grid grid-cols-2 sm:flex sm:items-center gap-2 flex-wrap bg-white dark:bg-gray-800 p-4 border-b",
         className
       )}
       {...rest}
